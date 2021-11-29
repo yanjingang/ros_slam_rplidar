fcad52ce8f6164e8371e5b77e8d829e8c5307551
yanjingang <yanjingang@mail.com>
2021-11-29 17:14:59 +0800

    format code

--- script/test.py
+++ script/test.py
@@ -94,19 +94,7 @@ class BaseControl:
         self.battery_topic = rospy.get_param('~battery_topic', 'battery')
         self.battery_freq = float(rospy.get_param('~battery_freq', '1'))
         self.cmd_vel_topic = rospy.get_param('~cmd_vel_topic', '/cmd_vel')
-        """
-        self.ackermann_cmd_topic = rospy.get_param('~ackermann_cmd_topic', '/ackermann_cmd_topic')
-
-        self.pub_imu = bool(rospy.get_param('~pub_imu', False))
-        if(self.pub_imu == True):
-            self.imuId = rospy.get_param('~imu_id', 'imu')
-            self.imu_topic = rospy.get_param('~imu_topic', 'imu')
-            self.imu_freq = float(rospy.get_param('~imu_freq', '50'))
-            if self.imu_freq > 100:
-                self.imu_freq = 100
-        self.pub_sonar = bool(rospy.get_param('~pub_sonar', False))
-        self.sub_ackermann = bool(rospy.get_param('~sub_ackermann', False))
-        """
+ 
 
         # define param
         self.current_time = rospy.Time.now()
@@ -160,28 +148,23 @@ class BaseControl:
             sys.exit(0)
         rospy.loginfo("Serial Open Succeed")
 
-        # if move base type is ackermann car like robot and use ackermann msg ,sud ackermann topic,else sub cmd_vel topic
-        """if(('NanoCar' in base_type) & (self.sub_ackermann == True)):
-            from ackermann_msgs.msg import AckermannDriveStamped
-            self.sub = rospy.Subscriber(self.ackermann_cmd_topic, AckermannDriveStamped, self.ackermannCmdCB, queue_size=20)
-        else:"""
+
         self.tf_broadcaster = tf.TransformBroadcaster()
+        # test sub
+        self.sub = rospy.Subscriber("/tank/cmd", Twist, self.subTankCmd, queue_size=20)
+        # test pub
+        self.pub = rospy.Publisher(self.odom_topic, Odometry, queue_size=10)
+        self.timer_odom = rospy.Timer(rospy.Duration(1.0/self.odom_freq), self.timerPubOdom)
+
+
+        """
         # 监听move_base发布给底盘的移动命令，并发送给底盘串口执行
         self.sub = rospy.Subscriber(self.cmd_vel_topic, Twist, self.cmdCB, queue_size=20)
         # 里程计数据发布对象
         self.pub = rospy.Publisher(self.odom_topic, Odometry, queue_size=10)
         # 电量数据发布对象
         self.battery_pub = rospy.Publisher(self.battery_topic, BatteryState, queue_size=3)
-        """if self.pub_sonar:
-            if sonar_num > 0:
-                self.timer_sonar = rospy.Timer(rospy.Duration(100.0/1000), self.timerSonarCB)
-                self.range_pub1 = rospy.Publisher('sonar_1', Range, queue_size=3)
-            if sonar_num > 1:
-                self.range_pub2 = rospy.Publisher('sonar_2', Range, queue_size=3)
-            if sonar_num > 2:
-                self.range_pub3 = rospy.Publisher('sonar_3', Range, queue_size=3)
-            if sonar_num > 3:
-                self.range_pub4 = rospy.Publisher('sonar_4', Range, queue_size=3)"""
+
         # 定频发布里程计数据
         self.timer_odom = rospy.Timer(rospy.Duration(1.0/self.odom_freq), self.timerOdomCB)
         # 定频发布电量数据
@@ -189,24 +172,65 @@ class BaseControl:
         # 定频通信
         self.timer_communication = rospy.Timer(rospy.Duration(1.0/500), self.timerCommunicationCB)
 
-        """
-        # inorder to compatibility old version firmware,imu topic is NOT pud in default
-        if(self.pub_imu):
-            self.timer_imu = rospy.Timer(rospy.Duration(1.0/self.imu_freq), self.timerIMUCB)
-            self.imu_pub = rospy.Publisher(self.imu_topic, Imu, queue_size=10)
-        """
         # 获取底盘硬件及固件版本
         self.getVersion()
-        # move base imu initialization need about 2s,during initialization,move base system is blocked
-        # so need this gap
-        time.sleep(2.2)
+        time.sleep(2.2)     # 等待底盘上的imu初始化
         # 获取底盘硬件序列号
         self.getSN()
         time.sleep(0.01)
         # 获取底盘信息
         self.getInfo()
-
+        """
     
+
+    # test sub cmd
+    def subTankCmd(self, data):
+        rospy.logerr("get /tank/cmd data! output: " + data)
+
+    # test pub odom
+    def timerPubOdom(self, event):
+        # calculate odom data
+        Vx = float(ctypes.c_int16(self.Vx).value/1000.0)
+        Vy = float(ctypes.c_int16(self.Vy).value/1000.0)
+        Vyaw = float(ctypes.c_int16(self.Vyaw).value/1000.0)
+
+        self.pose_yaw = float(ctypes.c_int16(self.Yawz).value/100.0)
+        self.pose_yaw = self.pose_yaw*math.pi/180.0
+
+        self.current_time = rospy.Time.now()
+        dt = (self.current_time - self.previous_time).to_sec()
+        self.previous_time = self.current_time
+        self.pose_x = self.pose_x + Vx * \
+            (math.cos(self.pose_yaw))*dt - Vy * (math.sin(self.pose_yaw))*dt
+        self.pose_y = self.pose_y + Vx * \
+            (math.sin(self.pose_yaw))*dt + Vy * (math.cos(self.pose_yaw))*dt
+
+        pose_quat = tf.transformations.quaternion_from_euler(
+            0, 0, self.pose_yaw)
+        msg = Odometry()
+        msg.header.stamp = self.current_time
+        msg.header.frame_id = self.odomId
+        msg.child_frame_id = self.baseId
+        msg.pose.pose.position.x = self.pose_x
+        msg.pose.pose.position.y = self.pose_y
+        msg.pose.pose.position.z = 0
+        msg.pose.pose.orientation.x = pose_quat[0]
+        msg.pose.pose.orientation.y = pose_quat[1]
+        msg.pose.pose.orientation.z = pose_quat[2]
+        msg.pose.pose.orientation.w = pose_quat[3]
+        msg.twist.twist.linear.x = Vx
+        msg.twist.twist.linear.y = Vy
+        msg.twist.twist.angular.z = Vyaw
+        self.pub.publish(msg)
+        self.tf_broadcaster.sendTransform(
+            (self.pose_x, self.pose_y, 0.0), pose_quat, self.current_time, self.baseId, self.odomId)
+
+
+
+
+
+
+
     # CRC-8 Calculate
     def crc_1byte(self, data):
         crc_1byte = 0
@@ -257,35 +281,6 @@ class BaseControl:
             rospy.logerr("Vel_cmd Command Send Faild! output: " + output)
         self.serialIDLE_flag = 0
 
-    """
-    # Subscribe ackermann Cmd call this to send vel cmd to move base
-    def ackermannCmdCB(self, data):
-        self.speed = data.drive.speed
-        self.steering_angle = data.drive.steering_angle
-        self.last_ackermann_cmd_time = rospy.Time.now()
-        output = chr(0x5a) + chr(12) + chr(0x01) + chr(0x15) + \
-            chr((int(self.speed*1000.0) >> 8) & 0xff) + chr(int(self.speed*1000.0) & 0xff) + \
-            chr(0x00) + chr(0x00) + \
-            chr((int(self.steering_angle*1000.0) >> 8) & 0xff) + chr(int(self.steering_angle*1000.0) & 0xff) + \
-            chr(0x00)
-        outputdata = [0x5a, 0x0c, 0x01, 0x15, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
-        outputdata[4] = (int(self.speed*1000.0) >> 8) & 0xff
-        outputdata[5] = int(self.speed*1000.0) & 0xff
-        outputdata[8] = (int(self.steering_angle*1000.0) >> 8) & 0xff
-        outputdata[9] = int(self.steering_angle*1000.0) & 0xff
-        crc_8 = self.crc_byte(outputdata, len(outputdata)-1)
-        output += chr(crc_8)
-        while self.serialIDLE_flag:
-            time.sleep(0.01)
-        self.serialIDLE_flag = 4
-        try:
-            while self.serial.out_waiting:
-                pass
-            self.serial.write(output)
-        except:
-            rospy.logerr("ackermann Vel Command Send Faild! output: " + output)
-        self.serialIDLE_flag = 0
-    """
 
     # 通信计时器回调 Communication Timer callback to handle receive data
     # depend on communication protocol
@@ -530,140 +525,6 @@ class BaseControl:
         msg.current = float(self.Icurrent/1000.0)
         self.battery_pub.publish(msg)
 
-    """
-    # Sonar Timer callback function to get battery info
-    def timerSonarCB(self, event):
-        output = chr(0x5a) + chr(0x06) + chr(0x01) + chr(0x19) + \
-            chr(0x00) + chr(0xff)  # 0xff is CRC-8 value
-        while(self.serialIDLE_flag):
-            time.sleep(0.01)
-        self.serialIDLE_flag = 3
-        try:
-            while self.serial.out_waiting:
-                pass
-            self.serial.write(output)
-        except:
-            rospy.logerr("Sonar Command Send Faild! output: " + output)
-        self.serialIDLE_flag = 0
-        msg = Range()
-        msg.header.stamp = self.current_time
-        msg.field_of_view = 0.26  # about 15 degree
-        msg.max_range = 2.5
-        msg.min_range = 0.01
-        # Sonar 1
-        msg.header.frame_id = 'Sonar_1'
-        if self.Sonar[0] == 0xff:
-            msg.range = float('inf')
-        else:
-            msg.range = self.Sonar[0] / 100.0
-        self.range_pub1.publish(msg)
-
-        # TF value calculate from mechanical structure
-        if('NanoRobot' in base_type):
-            self.tf_broadcaster.sendTransform(
-                (0.0, 0.0, 0.0), (0.0, 0.0, 0.0, 1.0), self.current_time, 'Sonar_1', self.baseId)
-        elif('NanoCar' in base_type):
-            self.tf_broadcaster.sendTransform(
-                (0.18, 0.0, 0.0), (0.0, 0.0, 0.0, 1.0), self.current_time, 'Sonar_1', self.baseId)
-        elif('4WD' in base_type):
-            self.tf_broadcaster.sendTransform(
-                (0.0, 0.0, 0.0), (0.0, 0.0, 0.0, 1.0), self.current_time, 'Sonar_1', self.baseId)
-        else:
-            pass
-
-        # Sonar 2
-        if sonar_num > 1:
-            if self.Sonar[1] == 0xff:
-                msg.range = float('inf')
-            else:
-                msg.range = self.Sonar[1] / 100.0
-            msg.header.frame_id = 'Sonar_2'
-            self.range_pub2.publish(msg)
-
-            if('NanoRobot' in base_type):
-                self.tf_broadcaster.sendTransform(
-                    (0.0, 0.0, 0.0), (0.0, 0.0, -1.0, 0), self.current_time, 'Sonar_2', self.baseId)
-            elif('NanoCar' in base_type):
-                self.tf_broadcaster.sendTransform(
-                    (-0.035, 0.0, 0.0), (0.0, 0.0, -1.0, 0), self.current_time, 'Sonar_2', self.baseId)
-            elif('4WD' in base_type):
-                self.tf_broadcaster.sendTransform(
-                    (0.0, 0.0, 0.0), (0.0, 0.0, -1.0, 0), self.current_time, 'Sonar_2', self.baseId)
-            else:
-                pass
-        if sonar_num > 2:
-            # Sonar 3
-            msg.header.frame_id = 'Sonar_3'
-            if self.Sonar[2] == 0xff:
-                msg.range = float('inf')
-            else:
-                msg.range = self.Sonar[2] / 100.0
-            self.range_pub3.publish(msg)
-            self.tf_broadcaster.sendTransform(
-                (0.0, 0.0, 0.0), (0.0, 0.0, 0.707, 0.707), self.current_time, 'Sonar_3', self.baseId)
-        if sonar_num > 3:
-            # Sonar 4
-            if self.Sonar[3] == 0xff:
-                msg.range = float('inf')
-            else:
-                msg.range = self.Sonar[3] / 100.0
-            msg.header.frame_id = 'Sonar_4'
-            self.range_pub4.publish(msg)
-            self.tf_broadcaster.sendTransform(
-                (0.0, 0.0, 0.0), (0.0, 0.0, -0.707, 0.707), self.current_time, 'Sonar_4', self.baseId)
-
-    # IMU Timer callback function to get raw imu info
-    def timerIMUCB(self, event):
-        output = chr(0x5a) + chr(0x06) + chr(0x01) + chr(0x13) + \
-            chr(0x00) + chr(0x33)  # 0x33 is CRC-8 value
-        while(self.serialIDLE_flag):
-            time.sleep(0.01)
-        self.serialIDLE_flag = 3
-        try:
-            while self.serial.out_waiting:
-                pass
-            self.serial.write(output)
-        except:
-            rospy.logerr("Imu Command Send Faild! output: " + output)
-
-        self.serialIDLE_flag = 0
-        msg = Imu()
-        msg.header.stamp = self.current_time
-        msg.header.frame_id = self.imuId
-
-        msg.angular_velocity.x = float(
-            ctypes.c_int32(self.Gyro[0]).value/100000.0)
-        msg.angular_velocity.y = float(
-            ctypes.c_int32(self.Gyro[1]).value/100000.0)
-        msg.angular_velocity.z = float(
-            ctypes.c_int32(self.Gyro[2]).value/100000.0)
-
-        msg.linear_acceleration.x = float(
-            ctypes.c_int32(self.Accel[0]).value/100000.0)
-        msg.linear_acceleration.y = float(
-            ctypes.c_int32(self.Accel[1]).value/100000.0)
-        msg.linear_acceleration.z = float(
-            ctypes.c_int32(self.Accel[2]).value/100000.0)
-
-        msg.orientation.w = float(ctypes.c_int16(self.Quat[0]).value/10000.0)
-        msg.orientation.x = float(ctypes.c_int16(self.Quat[1]).value/10000.0)
-        msg.orientation.y = float(ctypes.c_int16(self.Quat[2]).value/10000.0)
-        msg.orientation.z = float(ctypes.c_int16(self.Quat[3]).value/10000.0)
-
-        self.imu_pub.publish(msg)
-        # TF value calculate from mechanical structure
-        if('NanoRobot' in base_type):
-            self.tf_broadcaster.sendTransform(
-                (-0.062, -0.007, 0.08), (0.0, 0.0, 0.0, 1.0), self.current_time, self.imuId, self.baseId)
-        elif('NanoCar' in base_type):
-            self.tf_broadcaster.sendTransform(
-                (0.0, 0.0, 0.09), (0.0, 0.0, 0.0, 1.0), self.current_time, self.imuId, self.baseId)
-        elif('4WD' in base_type):
-            self.tf_broadcaster.sendTransform(
-                (-0.065, 0.0167, 0.02), (0.0, 0.0, 0.0, 1.0), self.current_time, self.imuId, self.baseId)
-        else:
-            pass
-    """
 
 # main function
 if __name__ == "__main__":
